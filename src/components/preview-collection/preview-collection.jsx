0148ee4f396886aca8523133e2e1f485949cdacd
Sashakil12 <send2shakil@gmail.com>
2019-11-20 16:17:26 +0600

    :added shop page, collection component collection preview page

--- src/components/preview-collection/preview-collection.jsx
+++ src/components/preview-collection/preview-collection.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './preview-collection.scss'
-
+import CollectionItem from '../collection-item/collection-item.components' 
 
 const CollectionPreview =({title, items})=>{
-    return (<div className="collectionPreview">
+    return (<div className="collection-preview">
         <h1 className="title">{title}</h1>
         <div className="preview">
-        {items.filter((i, idx)=>idx<4).map(i=>{
-            return (<div key={i.id}>{i.name}</div>)
+        {items.filter((i, idx)=>idx<4).map(({id, ...others})=>{
+            return (<CollectionItem key={id}{...others}></CollectionItem>)
 
         })}
 
