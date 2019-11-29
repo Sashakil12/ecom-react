f500af19339412a9e67aba7f071f29d2c5ce8000
Sashakil12 <send2shakil@gmail.com>
2019-11-29 23:51:34 +0600

    set up redux

--- src/components/header/header.components.jsx
+++ src/components/header/header.components.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import './header.styles.scss';
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crowns.svg'
@@ -9,26 +10,29 @@ const Header = ({currentUser})=>{
         <Link className="logo-container" to='/'>
             <Logo className="logo" />
         </Link>
-        <div className="options">
-            <Link className="option" to="/shop">
-                SHOP
-            </Link>
-            <Link className="option" to="/contact">
-                CONTACT
-            </Link>
-            {currentUser ?
-            <div className="option" onClick={()=>auth.signOut()}>
-                        SIGN OUT
+            <div className="options">
+                <Link className="option" to="/shop">
+                    SHOP
+                </Link>
+                <Link className="option" to="/contact">
+                    CONTACT
+                </Link>
+                {currentUser ?
+                    <div className="option" onClick={()=>auth.signOut()}>
+                                SIGN OUT
+                    </div>
+                :
+                    <Link className="option" to="/signin">
+                        SIGN IN
+                    </Link>
+                }
             </div>
-            :
-            <Link className="option" to="/signin">
-                SIGN IN
-            </Link>
-            }
-        </div>
     </div>)
 }
     
+const mapStateToProps = (state) =>({
+        currentUser: state.user.currentUser
+})
 
 
-export default Header;
+export default connect(mapStateToProps)(Header)
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-ReactDOM.render(<BrowserRouter>
-                    <App />
-                </BrowserRouter>
+import { Provider } from 'react-redux'; 
+import Store from './redux/store'
+ReactDOM.render(<Provider store={Store}>
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </Provider>
                 , document.getElementById('root'));
 
