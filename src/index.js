3b0cf39d571433e8ad1efe3b8827c8181d92e31a
Sashakil12 <send2shakil@gmail.com>
2019-12-02 19:27:23 +0600

    Implemented persistance in cart

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'; 
-import Store from './redux/store'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { Store, persistor } from './redux/store'
 ReactDOM.render(<Provider store={Store}>
                     <BrowserRouter>
-                        <App />
+                        <PersistGate loading={null} persistor={ persistor }>
+                            <App />
+                        </PersistGate>
                     </BrowserRouter>
                 </Provider>
                 , document.getElementById('root'));
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
+import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist';
 
 import rootReducer from './root-reducer'
 
 const middlewares = [logger];
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist:['cart']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const Store = createStore(persistedReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(Store)
 
-const Store = createStore(rootReducer, applyMiddleware(...middlewares))
-
-export default Store
